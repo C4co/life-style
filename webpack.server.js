9467462b2831aef4b3e0d87195a01efd5923ed43
c4co <carllos.nc@gmail.com>
2017-11-08 00:55:53 +0000

    Reconfiguring webpack

--- webpack.server.js
+++ webpack.server.js
@@ -1,10 +1,17 @@
 // https://www.browsersync.io/docs/options#option-port
 
+const modRewrite = require('connect-modrewrite');
+
 module.exports = {
   open: false,
   files: ['*.js', '*.jsx', '*.scss', '*.html', './**/*.html'],
   port: '3000',
   server: {
-    baseDir: './'
+    baseDir: './',
+    middleware: [
+      modRewrite([
+        '!\\.\\w+$ /index.html [L]'
+      ])
+    ]
   }
 }
