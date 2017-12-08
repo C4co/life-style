48887563305531334e58111fbf3ce9f1b5bf00f5
c4co <carllos.nc@gmail.com>
2017-12-08 04:47:00 +0000

    Adding image component

---
+++ src/components/image/image.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+import {h} from 'preact'
+import styleGuide from 'style.guide'
+
+const patternBackground = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAGklEQVQYlWO4d+/ef3TMgA0MBYWDzDkUKQQADFmz4yjA0qgAAAAASUVORK5CYII="
+
+const ImageWrapper = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+
+  figure{
+    border: solid ${styleGuide.borderColor} 1px;
+    padding: 20px;
+    background: url(${patternBackground});
+  };
+
+  h1{
+    font-size: 0.9em;
+    font-weight: normal;
+    border: solid ${styleGuide.borderColor} 1px;
+    background: ${styleGuide.contrast};
+    border-top: none;
+    border-radius: 0px 0px 5px 5px;
+    padding: 10px;
+  }
+`
+
+const Image = ({children, ...props}) => (
+  <ImageWrapper>
+    <figure>
+      <img src={props.src} alt='image' />
+    </figure>
+    <h1> {props.src} </h1>
+  </ImageWrapper>
+)
+
+export default Image
