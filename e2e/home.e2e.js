9b996f2e028d3000bc4398c8f0a68d78da91bcec
c4co <carllos.nc@gmail.com>
2017-12-24 03:17:16 +0000

    Adding new test cases

--- e2e/home.e2e.js
+++
@@ -1,7 +0,0 @@
-Feature('Hello suite')
-
-Scenario('Looking for initial message', (I) => {
-  I.amOnPage('/')
-  I.see('Hello webpack')
-  I.wait(1000)
-})
---
+++ e2e/pages.e2e.js
@@ -0,0 +1,31 @@
+Feature('Pages suite')
+
+Scenario('Looking for all defined pages', (I) => {
+  I.amOnPage('/')
+  I.see('LIFESTYLE')
+  I.wait(1)
+
+  I.amOnPage('/type')
+  I.see('TYPOGRAPHY')
+  I.wait(1)
+
+  I.amOnPage('/forms')
+  I.see('FORMS')
+  I.wait(1)
+
+  I.amOnPage('/colors')
+  I.see('COLORS')
+  I.wait(1)
+
+  I.amOnPage('/buttons')
+  I.see('BUTTONS')
+  I.wait(1)
+
+  I.amOnPage('/tables')
+  I.see('TABLES')
+  I.wait(1)
+
+  I.amOnPage('/images')
+  I.see('IMAGES')
+  I.wait(1)
+})
