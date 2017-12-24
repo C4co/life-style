2dd09f5bbf6027e8de27e7aad7a0634ec5a01eca
c4co <carllos.nc@gmail.com>
2017-12-24 04:51:09 +0000

    small adjusts

--- e2e/pages.e2e.js
+++ e2e/pages.e2e.js
@@ -1,31 +1,33 @@
 Feature('Pages suite')
 
 Scenario('Looking for all defined pages', (I) => {
-  I.amOnPage('/')
-  I.see('Hello')
+  // I.amOnPage('/')
+  // I.see('Hello')
+  // I.wait(1)
+
+  I.wait(3)
+
+  I.amOnPage('/type')
+  I.see('TYPOGRAPHY')
   I.wait(1)
 
-  // I.amOnPage('/type')
-  // I.see('TYPOGRAPHY')
-  // I.wait(1)
+  I.amOnPage('/forms')
+  I.see('FORMS')
+  I.wait(1)
 
-  // I.amOnPage('/forms')
-  // I.see('FORMS')
-  // I.wait(1)
+  I.amOnPage('/colors')
+  I.see('COLORS')
+  I.wait(1)
 
-  // I.amOnPage('/colors')
-  // I.see('COLORS')
-  // I.wait(1)
+  I.amOnPage('/buttons')
+  I.see('BUTTONS')
+  I.wait(1)
 
-  // I.amOnPage('/buttons')
-  // I.see('BUTTONS')
-  // I.wait(1)
+  I.amOnPage('/tables')
+  I.see('TABLES')
+  I.wait(1)
 
-  // I.amOnPage('/tables')
-  // I.see('TABLES')
-  // I.wait(1)
-
-  // I.amOnPage('/images')
-  // I.see('IMAGES')
-  // I.wait(1)
+  I.amOnPage('/images')
+  I.see('IMAGES')
+  I.wait(1)
 })
