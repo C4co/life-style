16b3b954160e5ac41293f0d3a04a341103012150
c4co <carllos.nc@gmail.com>
2018-04-07 20:50:59 +0100

    Fixing image paths.

--- src/pages/images/images.js
+++ src/pages/images/images.js
@@ -2,11 +2,12 @@ import {h, render, Component} from 'preact'
 import Page from '../../layouts/page/page'
 import Block from '../../components/block/block'
 import Image from '../../components/image/image'
+import lifestyleDarkImage from '../../../assets/lifestyle-dark.svg'
 
 const Images = ({children, ...props}) => (
   <Page title='Images' description='Lifestyle images'>
     <Block title='Logo'>
-      <Image src='assets/lifestyle-dark.svg' />
+      <Image src={lifestyleDarkImage} />
     </Block>
   </Page>
 )
