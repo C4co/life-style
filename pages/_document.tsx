11c40690eedeee53217819f368c3d378d4a004d1
Carlos Costa <carllos.nc@gmail.com>
2021-11-13 00:57:58 -0300

    Adding data-theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html data-theme="light">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
