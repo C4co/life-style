e925ae953bc258a7cc70f275bc59aed6b294d392
Carlos Costa <carllos.nc@gmail.com>
2021-11-13 00:57:44 -0300

    Update header component.

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,9 +3,24 @@ type Props = {
 }
 
 export function Header({ children }: Props){
+
+  function setTheme(theme: string){
+    const element = document.querySelector('[data-theme]')
+    element.setAttribute('data-theme', theme)
+  }
+
   return (
     <header className='header'>
       <h1 className="title--mid"> { children } </h1>
+
+      <div className="header__buttons">
+        <button className="header__button" onClick={() => { setTheme('light') }}>
+          🌞
+        </button>
+        <button className="header__button" onClick={() => { setTheme('dark') }}>
+          🌚
+        </button>
+      </div>
     </header>
   )
 }
